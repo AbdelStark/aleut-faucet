4aa8d2623faba0fba4c789903290a72f4d53b89c
Abdelhamid Bakhta <abdelhamid.bakhta@consensys.net>
2021-04-19 11:50:46 +0200

    Init Aleut Faucet

---
+++ src/App.vue
@@ -0,0 +1,52 @@
+<template>
+  <v-app>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+      <div class="d-flex align-center">
+        Aleut Faucet
+      </div>
+
+      <v-spacer></v-spacer>
+
+      <v-btn
+        href="http://eip1559-testnet.ops.pegasys.tech:3000/"
+        target="_blank"
+        text
+      >
+        <span class="mr-2">Explorer</span>
+        <v-icon>mdi-open-in-new</v-icon>
+      </v-btn>
+      <v-btn
+          href="http://eip1559-testnet.ops.pegasys.tech:3001/"
+          target="_blank"
+          text
+      >
+        <span class="mr-2">Network</span>
+        <v-icon>mdi-open-in-new</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-main>
+      <Faucet/>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import Faucet from './components/Faucet';
+
+export default {
+  name: 'App',
+
+  components: {
+    Faucet,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ src/components/Faucet.vue
@@ -0,0 +1,84 @@
+<template>
+  <v-container>
+    <v-row class="text-center">
+      <v-col cols="12">
+        <v-img
+            :src="require('../assets/logo.svg')"
+            class="my-3"
+            contain
+            height="200"
+        />
+      </v-col>
+
+      <v-col class="mb-4" cols="12">
+        <h1 class="display-2 font-weight-bold mb-3">
+          Welcome to Aleut Faucet
+        </h1>
+      </v-col>
+      <v-row justify="center">
+        <v-col
+            class="mb-2"
+            cols="4"
+        >
+          <v-text-field
+              flat solo placeholder="Type your Ethereum address"
+              hide-details="auto"
+              v-model="recipient"
+          ></v-text-field>
+        </v-col>
+      </v-row>
+
+      <v-col
+          class="mb-2"
+          cols="12"
+      >
+        <v-btn color="indigo accent-4"
+
+        >Request 5 ETH
+        </v-btn>
+      </v-col>
+      <v-col
+          class="mb-5"
+          cols="12"
+      >
+        <h2 class="headline font-weight-bold mb-3">
+          Important Links
+        </h2>
+
+        <v-row justify="center">
+          <a
+              v-for="(link, i) in importantLinks"
+              :key="i"
+              :href="link.href"
+              class="subheading mx-3"
+              target="_blank"
+          >
+            {{ link.text }}
+          </a>
+        </v-row>
+      </v-col>
+
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'Faucet',
+
+  data: () => ({
+    recipient: null,
+    importantLinks: [
+      {
+        text: 'EIP-1559 Resources',
+        href: 'https://hackmd.io/@timbeiko/1559-updates/https%3A%2F%2Fhackmd.io%2F%40timbeiko%2F1559-resources',
+      },
+    ],
+  }),
+  methods: {
+    requestFaucet() {
+
+    },
+  }
+}
+</script>
